c3bf13551f88f119c119448c5761f5928755661d
Volkmar Rigo <volkmar@rigo.bz>
2023-12-16 19:23:48 +0100

    Part 2 - not working

--- Day14/Day14Solver.cs
+++ Day14/Day14Solver.cs
@@ -9,7 +9,7 @@ public class Day14Tests
     public void Step1WithExample() => new Day14Solver().ExecuteExample1("136");
 
     [Fact]
-    public void Step2WithExample() => new Day14Solver().ExecuteExample2("??");
+    public void Step2WithExample() => new Day14Solver().ExecuteExample2("64");
 
     [Fact]
     public void Step1WithPuzzleInput() => _output.WriteLine(new Day14Solver().ExecutePuzzle1());
@@ -20,44 +20,133 @@ public class Day14Tests
 
 public class Day14Solver : SolverBase
 {
-    char[,] _data;
+    private int _width;
+    private int _height;
+    private char[,] _map1;
+    private char[,] _map2;
+    private int[] _firstFree;
 
     protected override void Parse(List<string> data)
     {
-        _data = new char[data[0].Length, data.Count];
-        for (int y = 0; y < data.Count; y++)
-        for (int x = 0; x < data[y].Length; x++)
-            _data[x, y] = data[y][x];
+        _width = data[0].Length;
+        _height = data.Count;
+        _map1 = new char[_width, _height];
+        for (int y = 0; y < _height; y++)
+        for (int x = 0; x < _width; x++)
+            _map1[x, y] = data[y][x];
+
+        _map2 = new char[_width, _height];
+        _firstFree = new int[_width];
     }
 
-    char[,] Tilt(char[,] map)
+    void InitFree()
     {
-        var newMap = new char[map.GetLength(0), map.GetLength(1)];
-        var firstFreeY = new int[map.GetLength(0)];
-        for (int x = 0; x < firstFreeY.Length; x++)
-            firstFreeY[x] = -1;
+        for (var i = 0; i < _width; i++)
+            _firstFree[i] = -1;
+    }
 
-        for (int y = 0; y < map.GetLength(1); y++)
-        for (int x = 0; x < map.GetLength(0); x++)
+    void SetFirstFree(int index, int currentPos, char item)
+    {
+        if (item == '.' && _firstFree[index] == -1)
+            _firstFree[index] = currentPos;
+        else if (item != '.')
+            _firstFree[index] = -1;
+    }
+
+    void TiltNorth(char[,] map, char[,] newMap)
+    {
+        InitFree();
+
+        for (int y = 0; y < _height; y++)
+        for (int x = 0; x < _width; x++)
         {
             var item = map[x, y];
-            if (item == 'O' && firstFreeY[x] > -1)
+            if (item == 'O' && _firstFree[x] > -1)
             {
-                newMap[x, firstFreeY[x]] = item;
+                newMap[x, _firstFree[x]] = item;
                 newMap[x, y] = '.';
-                firstFreeY[x]++;
+                _firstFree[x]++;
             }
             else
             {
                 newMap[x, y] = item;
-                if (item == '.' && firstFreeY[x] == -1)
-                    firstFreeY[x] = y;
-                else if (item != '.')
-                    firstFreeY[x] = -1;
+                SetFirstFree(x, y, item);
             }
         }
+    }
 
-        return newMap;
+    void TiltWest(char[,] map, char[,] newMap)
+    {
+        InitFree();
+
+        for (int x = 0; x < _width; x++)
+        for (int y = 0; y < _height; y++)
+        {
+            var item = map[x, y];
+            if (item == 'O' && _firstFree[y] > -1)
+            {
+                newMap[_firstFree[y], y] = item;
+                newMap[x, y] = '.';
+                _firstFree[y]++;
+            }
+            else
+            {
+                newMap[x, y] = item;
+                SetFirstFree(y, x, item);
+            }
+        }
+    }
+
+    void TiltSouth(char[,] map, char[,] newMap)
+    {
+        InitFree();
+
+        for (int y = _height - 1; y >= 0; y--)
+        for (int x = 0; x < _width; x++)
+        {
+            var item = map[x, y];
+            if (item == 'O' && _firstFree[x] > -1)
+            {
+                newMap[x, _firstFree[x]] = item;
+                newMap[x, y] = '.';
+                _firstFree[x]--;
+            }
+            else
+            {
+                newMap[x, y] = item;
+                SetFirstFree(x, y, item);
+            }
+        }
+    }
+
+    void TiltEast(char[,] map, char[,] newMap)
+    {
+        InitFree();
+
+        for (int x = _width - 1; x >= 0; x--)
+        for (int y = 0; y < _height; y++)
+        {
+            var item = map[x, y];
+            if (item == 'O' && _firstFree[y] > -1)
+            {
+                newMap[_firstFree[y], y] = item;
+                newMap[x, y] = '.';
+                _firstFree[y]--;
+            }
+            else
+            {
+                newMap[x, y] = item;
+                SetFirstFree(y, x, item);
+            }
+        }
+    }
+
+    void TiltFull()
+    {
+        TiltNorth(_map1, _map2);
+        TiltWest(_map2, _map1);
+        TiltSouth(_map1, _map2);
+        TiltEast(_map2, _map1);
     }
 
     void Dump(char[,] map)
@@ -87,10 +176,17 @@ public class Day14Solver : SolverBase
         return result;
     }
 
-    protected override object Solve1() => TotalLoad(Tilt(_data));
+    protected override object Solve1()
+    {
+        TiltNorth(_map1, _map2);
+        return TotalLoad(_map2);
+    }
 
     protected override object Solve2()
     {
-        throw new Exception("Solver error");
+        for (int i = 0; i < 1_000_000_000; i++)
+            TiltFull();
+
+        return TotalLoad(_map1);
     }
 }
